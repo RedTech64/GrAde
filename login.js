85dd023a904456d60f1b6e0617975d7efe0eb252
Cole Weinman <coleweinman@gmail.com>
2018-04-26 17:07:27 -0500

    Removed sensitive information.

--- login.js
+++ login.js
@@ -1,10 +1,8 @@
 var config = {
-    apiKey: "AIzaSyDFCvOGyGSnnJhyb-isENFCCzuhXhGFO9k",
     authDomain: "redtech-grade.firebaseapp.com",
     databaseURL: "https://redtech-grade.firebaseio.com",
     projectId: "redtech-grade",
-    storageBucket: "redtech-grade.appspot.com",
-    messagingSenderId: "219651988629"
+    storageBucket: "redtech-grade.appspot.com"
 };
 firebase.initializeApp(config);
 var provider = new firebase.auth.GoogleAuthProvider();
